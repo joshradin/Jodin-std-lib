f50cfc0a810c3b6154e27ba866e58a4410d0876f
Joshua Radin <jradn16@gmail.com>
2020-05-06 00:58:12 -0400

    fixed stream virtual order

--- include/stream.h
+++ include/stream.h
@@ -3,46 +3,46 @@
 
 in std
 class Stream {
-	public virtual void close();
+	virtual public void close();
 };
 
 in std
 class OutputStream : Stream {
 
-	public virtual void write(char c);
-	public virtual void write(std::String s);
-	public virtual void write(std::Object o);
-	public virtual void flush();
+	virtual public void write(char c);
+	virtual public void write(std::String s);
+	virtual public void write(std::Object o);
+	virtual public void flush();
 
 };
 
 in std
 class OutputWriter : OutputStream {
 
-	public virtual void writeln();
-	public virtual void writeln(char c);
-	public virtual void writeln(std::String s);
-	public virtual void writeln(std::Object o);
+	virtual public void writeln();
+	virtual public void writeln(char c);
+	virtual public void writeln(std::String s);
+	virtual public void writeln(std::Object o);
 
 
 };
 
 in std
 class InputStream : Stream {
-	public virtual u8 read();
-	public virtual bool safe_read(u8* c);
-	public virtual bool read(u8 buffer[], int len);
-}
+	virtual public u8 read();
+	virtual public bool safe_read(u8* c);
+	virtual public bool read(u8 buffer[], int len);
+};
 
 in std
 class InputReader : InputStream {
 
-	public virtual char readChar();
-	public virtual std::String readString(int length);
-	public virtual std::String readLine();
-	public virtual std::String readToken();
+	virtual public char readChar();
+	virtual public std::String readString(int length);
+	virtual public std::String readLine();
+	virtual public std::String readToken();
 
-}
+};
 
 
 
