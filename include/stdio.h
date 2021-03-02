08feb4eb5ea2a4c90807a72e75f655bbaa25fc7b
Joshua Radin <54867459+JoshuaRadin37@users.noreply.github.com>
2021-03-02 15:36:25 -0500

    added all functions for the file reader and writer

--- include/stdio.h
+++ include/stdio.h
@@ -24,9 +24,6 @@ int fflush(FILE stream);
 int fgetpos(FILE stream, fpos_t* pos);
 FILE fopen(const char* filename, const char* mode);
 
-
-
-
 in std
 class FileWriter : OutputWriter {
 	private FILE f;
@@ -35,13 +32,41 @@ class FileWriter : OutputWriter {
 	public FileWriter(FILE f);
 	public FileWriter(FILE f, bool autoFlush);
 
+	public void writeln();
+	public void write(char c);
+	public void writeln(char c);
+
+	public void write(std::String s);
+	public void writeln(std::String s);
+
+	public void write(std::Object o);
+	public void writeln(std::Object o);
+
+	public void flush();
+	public void close();
+
 };
 
 in std
 class FileReader : InputReader {
 	FILE f;
 
-	public FileReader(FILE f);
+	public FileReader(FILE f)
+
+	public void close();
+	public u8 read();
+
+	public bool safe_read(u8* c);
+
+	public bool read(u8 buffer[], int len);
+
+	public char readChar();
+
+	public String readString(int len);
+
+	public String readLine();
+
+	public String readToken();
 };
 
 in std
@@ -50,7 +75,5 @@ class ContinuousReader : FileReader {
 	public ContinuousReader(FILE f);
 	virtual public char readChar();
 };
-
-
 
 #endif
